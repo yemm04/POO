35cfdc1ae46f3469158519dfc9b45b0d6a43a13b
yemm04 <yoshitomochizaki0@gmail.com>
2024-09-25 09:46:06 +0200

    Update Profesor.java

--- Documents/Lenguaje_Java_ciclo3_JeffersonParedesPretel/ProyectoFinal/src/proyectofinal/Profesor.java
+++ Documents/Lenguaje_Java_ciclo3_JeffersonParedesPretel/ProyectoFinal/src/proyectofinal/Profesor.java
@@ -1,6 +1,39 @@
 
 package proyectofinal;
 
-public class Profesor {
+public class Profesor extends Persona{
+     
+    private String asignatura;
+    private String disponibilidad;
     
+    public Profesor(int codigo, String nombre, int edad, String asignatura, String disponibilidad) {
+        super(codigo, nombre, edad);
+       
+        this.disponibilidad =disponibilidad;
+        this.asignatura = asignatura;
+    }
+
+    public String getAsignatura() {
+        return asignatura;
+    }
+
+    public void setAsignatura(String asignatura) {
+        this.asignatura = asignatura;
+    }
+
+    public String getdisponibilidad() {
+        return disponibilidad;
+    }
+
+    public void setDisponibilidad(String disponibilidad) {
+        this.disponibilidad = disponibilidad;
+    }
+
+
+
+    @Override
+    public void mostrarInformacion() {
+        System.out.println("Profesor: " + getNombre() + ", Código: " + getCodigo() + ", edad : " + getEdad() + ", Asignatura: " + asignatura + ", Disponibilidad:  " + disponibilidad );
+    }
 }
+    
