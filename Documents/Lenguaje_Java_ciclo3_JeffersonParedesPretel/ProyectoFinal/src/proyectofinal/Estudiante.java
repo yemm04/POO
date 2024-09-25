e6704ccdb55f89ad6e5beb9fc388f79cbb59e6f8
yemm04 <yoshitomochizaki0@gmail.com>
2024-09-25 09:45:07 +0200

    Update Estudiante.java

--- Documents/Lenguaje_Java_ciclo3_JeffersonParedesPretel/ProyectoFinal/src/proyectofinal/Estudiante.java
+++ Documents/Lenguaje_Java_ciclo3_JeffersonParedesPretel/ProyectoFinal/src/proyectofinal/Estudiante.java
@@ -1,6 +1,24 @@
 
 package proyectofinal;
 
-public class Estudiante {
-    
+class Alumno extends Persona {
+    private int tutoriasTomadas;
+
+    public Alumno(int codigo, String nombre, int edad, int tutoriasTomadas) {
+        super(codigo, nombre,edad);
+        this.tutoriasTomadas = tutoriasTomadas;
+    }
+
+    public int getTutoriasTomadas() {
+        return tutoriasTomadas;
+    }
+
+    public void setTutoriasTomadas(int TutoriasTomadas) {
+        this.tutoriasTomadas = TutoriasTomadas;
+    }
+
+    @Override
+    public void mostrarInformacion() {
+        System.out.println("Alumno: " + getNombre() + ", Código: " + getCodigo() + ", edad : " + getEdad() +  " ,TutoriasTomadas: " + tutoriasTomadas);
+    }
 }
