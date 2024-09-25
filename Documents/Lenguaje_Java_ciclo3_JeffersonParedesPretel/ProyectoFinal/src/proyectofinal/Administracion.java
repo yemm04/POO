02b4ef6b93e995107e85548538d435cdc6ca6b25
yemm04 <yoshitomochizaki0@gmail.com>
2024-09-25 09:46:41 +0200

    Update Administracion.java

--- Documents/Lenguaje_Java_ciclo3_JeffersonParedesPretel/ProyectoFinal/src/proyectofinal/Administracion.java
+++ Documents/Lenguaje_Java_ciclo3_JeffersonParedesPretel/ProyectoFinal/src/proyectofinal/Administracion.java
@@ -1,13 +1,49 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package proyectofinal;
 
-/**
- *
- * @author cliente
- */
-public class Administracion {
-    
+
+public class Administracion extends Persona {
+    private String fechaGestion;
+    private String descripcion;
+    private String tipoGestion;
+
+    public Administracion(int codigo, String nombre,int edad, String fechaGestion, String descripcion, String tipoGestion) {
+        super(codigo, nombre, edad);
+        this.fechaGestion = fechaGestion;
+        this.descripcion = descripcion;
+        this.tipoGestion = tipoGestion;
+    }
+
+    public String getFechaGestion() {
+        return fechaGestion;
+    }
+
+    public void setFechaGestion(String fechaGestion) {
+        this.fechaGestion = fechaGestion;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getTipoGestion() {
+        return tipoGestion;
+    }
+
+    public void setTipoGestion(String tipoGestion) {
+        this.tipoGestion = tipoGestion;
+    }
+
+    @Override
+    public void mostrarInformacion() {
+        System.out.println("Administración: " + getNombre() + ", Código: " + getCodigo() + ", edad : " + getEdad()+ ", Fecha de Gestión: " + fechaGestion + ", Descripción: " + descripcion + ", Tipo de Gestión: " + tipoGestion);
+    }
 }
+    
+    
+
+
