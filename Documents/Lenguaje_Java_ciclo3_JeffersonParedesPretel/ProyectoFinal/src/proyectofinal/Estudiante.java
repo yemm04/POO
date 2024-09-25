671df58774329e23825ccda8bd59772747e7cbe0
JeffersonParedes <valentinopp.2005@gmail.com>
2024-09-25 02:04:24 -0500

    Carga1

---
+++ Documents/Lenguaje_Java_ciclo3_JeffersonParedesPretel/ProyectoFinal/src/proyectofinal/Estudiante.java
@@ -0,0 +1,6 @@
+
+package proyectofinal;
+
+public class Estudiante {
+    
+}
---
+++ Documents/Lenguaje_Java_ciclo3_JeffersonParedesPretel/ProyectoFinal/src/proyectofinal/ProyectoFinal.java
@@ -0,0 +1,11 @@
+
+package proyectofinal;
+
+public class ProyectoFinal {
+
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
